f81a2fe7d3b28ca3cb5f04ae801a12fccd0573da
Dmitry Zhelnin <dmitry.zhelnin@gmail.com>
2018-08-13 16:30:00 +0300

    increase timeout for api requests

--- config/config.exs
+++ config/config.exs
@@ -2,6 +2,8 @@
 # and its dependencies with the aid of the Mix.Config module.
 use Mix.Config
 
+config :neuron, :neuron_connection_opts, recv_timeout: 10_000
+
 # This configuration is loaded before any dependency and is restricted
 # to this project. If another project depends on this project, this
 # file won't be loaded nor affect the parent project. For this reason,
