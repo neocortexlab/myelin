7855afa7627951cae0bd8c84cf9b9abd5b6a889e
Dmitry Zhelnin <dmitry.zhelnin@gmail.com>
2018-08-16 21:21:39 +0300

    start_process function

--- lib/myelin/agent.ex
+++ lib/myelin/agent.ex
@@ -2,7 +2,14 @@ defmodule Myelin.Agent do
   defmacro agent(address, do: block) do
     quote do
       defmodule unquote(String.to_atom("Elixir." <> address)) do
+        alias Pallium.Env
+
         @self unquote(address)
+
+        defp start_process(fun) do
+          Env.start_process(@self, fun)
+        end
+
         unquote(block)
       end
     end
