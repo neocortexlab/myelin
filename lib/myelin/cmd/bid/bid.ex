fe2537501d6f651e136c6624c3cbeb4c054e8b44
Dmitry Zhelnin <dmitry.zhelnin@gmail.com>
2018-10-05 02:10:59 +0300

    update bid fields

--- lib/myelin/cmd/bid/bid.ex
+++ lib/myelin/cmd/bid/bid.ex
@@ -21,13 +21,13 @@ defmodule Myelin.Cmd.Bid do
   # @fields ~w(ip net cluster device_name device_type memory_limit)a
   defp bid(address) do
     %Bid{
-      ip: "127.0.0.1",
-      net_id: "net_id",
+      agent_address: address,
       cluster_id: "cluster_id",
+      device_desc: "",
       device_name: "NVIDIA",
-      device_type: :gpu,
-      memory: 1000000000,
-      agent_address: address
+      device_type: :cpu,
+      memory_limit: 1_000_000,
+      node_id: ""
     }
   end
 end
